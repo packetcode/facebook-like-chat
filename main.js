0c490ea0ab7a02498ecf32b15f70b8264abd458c
kamaltejag <kamaltejag@gmail.com>
2020-05-16 11:56:42 +0530

    Added Files

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $(".chat_header").click(function() {
+    $(".chat_content").slideToggle("slow");
+  });
+
+  $(".message_header").click(function() {
+    $(".message_content").slideToggle("slow");
+    $(".input_box").toggle("slow");
+  });
+
+  $(".close").click(function() {
+    $(".message_box").hide();
+  });
+
+  $(".user").click(function() {
+    $(".message_box").show();
+    $(".message_content").show();
+    $(".input_box").show();
+  });
+
+  // Arrow Button
+  $(".enter").click(function() {
+    var msg = $(".message_input").val();
+    if (msg != "") {
+      $(".new_messages").append('<p class="my_message">' + msg + "</p>");
+      $(".message_input").val("");
+    }
+  });
+});
